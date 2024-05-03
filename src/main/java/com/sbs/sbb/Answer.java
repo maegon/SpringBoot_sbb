b6dc926d17faf626c19fa00c4667a6d7ca9f16d0
maegon <dlagorhs99@naver.com>
2024-05-03 15:43:56 +0900

    Answer Entity

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,27 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // answer 테이블
+public class Answer {
+    @Id // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+//  나중에 게시판 번호 같은거 만들었을때 게시판번호를 지정해주지 않으면 null값이 들어갈 수 있기 때문에
+//    private Integer questionId;
+    @ManyToOne // 1대다 관계 One: question, Many: answer
+    private Question question;
+
+}
