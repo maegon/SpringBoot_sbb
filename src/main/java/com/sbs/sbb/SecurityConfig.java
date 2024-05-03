5efb51b0cdd7580c22699ec27e2453668c439710
maegon <dlagorhs99@naver.com>
2024-05-03 14:21:01 +0900

    시큐리티 허용

---
+++ src/main/java/com/sbs/sbb/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sbs.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+//    모든 접근권한을 허용함(코드 작업할때 피곤해질 일을 방지하기 위함)
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        ;
+        return http.build();
+    }
+}
