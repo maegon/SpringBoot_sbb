5cef861b25b65db152ef31845c9f19ca0e8206bd
maegon <dlagorhs99@naver.com>
2024-05-07 15:27:52 +0900

    findBySubject 실행

--- src/main/java/com/sbs/sbb/QuestionRepository.java
+++ src/main/java/com/sbs/sbb/QuestionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
+    //findBySubject
+    Question findBySubject(String s);
 }
