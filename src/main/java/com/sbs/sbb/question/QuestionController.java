ae5dd64f48ee1348c6f00eff658537e51efa05d5
maegon <dlagorhs99@naver.com>
2024-05-08 16:40:23 +0900

    질문ì질문 상세 페이지 추가, 질문ì 상세 링크 수정

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -16,5 +17,13 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+
+    // id값을 받아 @PathVariable을 통해 id를 detail함수 안에서도 쓸수 있게 설정
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("id", id);
+        return "question_detail";
     }
 }
