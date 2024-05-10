b18d92facc7d651cfc96f6215d907dfdcba7055d
maegon <dlagorhs99@naver.com>
2024-05-10 14:35:39 +0900

    Validation 추가 및 유효성 검사 자동으로 체크

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -1,8 +1,10 @@
 package com.sbs.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/question")
+// @Validated 컨트롤러에서는 생략가능, 다른데선 Validation 라이브러리 사용시 써줘야함
 public class QuestionController {
     private final QuestionService questionService;
     @GetMapping("/list")
@@ -36,11 +39,11 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String questionCreate(QuestionForm questionForm) {
-        String subject = questionForm.getSubject();
-        String content = questionForm.getContent();
+    // validation 라이브러리 사용시 해당 함수에 @Valid 꼭 붙여줘야 유효성 검사를 함
+    // QuestionForm 값을 바인딩 할 때 유효성 체크를 해라!!
+    public String questionCreate(@Valid QuestionForm questionForm) {
 
-        Question q = this.questionService.create(subject, content);
+        Question q = this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 
         return "redirect:/question/list";
     }
