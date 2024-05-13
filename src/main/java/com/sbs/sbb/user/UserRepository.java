88afc710f93150e351efa74872b9d66620612a9e
maegon <dlagorhs99@naver.com>
2024-05-13 16:46:32 +0900

    오타 수정

--- src/main/java/com/sbs/sbb/user/UserRepository.java
+++ src/main/java/com/sbs/sbb/user/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
     @Transactional
     @Modifying
-    @Query(value="ALTER TABLE user AUTO_INCREMENT = 1", nativeQuery = true)
+    @Query(value="ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
 }
