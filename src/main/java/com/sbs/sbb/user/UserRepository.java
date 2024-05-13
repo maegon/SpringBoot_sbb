0e5bf1f0a334897d2f2e65f1aaeb406665d1b4f4
maegon <dlagorhs99@naver.com>
2024-05-13 16:42:35 +0900

    회원 엔티티, 회원 리포지터리, 회원 서비스 생성

---
+++ src/main/java/com/sbs/sbb/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sbs.sbb.user;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<SiteUser, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value="ALTER TABLE user AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
+}
