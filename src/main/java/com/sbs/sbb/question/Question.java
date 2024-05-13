36538f21b95ae7c692a678c27f0e0b4ef7129845
maegon <dlagorhs99@naver.com>
2024-05-13 17:55:47 +0900

    정답ì인지는 모르겠으나... 오류는 해결

--- src/main/java/com/sbs/sbb/question/Question.java
+++ src/main/java/com/sbs/sbb/question/Question.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Setter
 @Entity // question 테이블
 @ToString
-
 public class Question {
     @Id // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
@@ -29,11 +28,11 @@ public class Question {
 
     private LocalDateTime createDate;
 
-//  mappedBy Answer 클래스의 question 변수 이름을 적어야 함
-//  CascadeType.REMOVE 를 하면 question을 삭제할 때 답변도 같이 삭제한다는 뜻
-//  OneToMany는 테이블의 컬럼으로 생성되지 않음, 선택 작성
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    // mappedBy Answer 클래스의 question 변수 이름을 적어야 함
+    // CascadeType.REMOVE 를 하면 question을 삭제할 때 답변도 같이 삭제한다는 뜻
+    // OneToMany는 테이블의 컬럼으로 생성되지 않음, 선택 작성
     @LazyCollection(LazyCollectionOption.EXTRA)
     // answerList.size(); 함수가 실행될 때 SELECT COUNT가 실행됨
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
 }
--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -49,6 +49,7 @@ class SbbApplicationTests {
 
 		// 모든 데이터 삭제
 		// 흔적 삭제 -> 다음번 INSERT를 할 때 id가 1번으로 설정되도록
+		userRepository.deleteAll();
 		userRepository.clearAutoIncrement();
 
 		// 회원 2명 생성
