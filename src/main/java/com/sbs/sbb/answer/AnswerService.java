87fb8405daace1a687e3163cd5a65d48ca51956e
maegon <dlagorhs99@naver.com>
2024-05-14 18:14:50 +0900

    답변 수정, 삭제 기능 추가(answer_form 추가)

--- src/main/java/com/sbs/sbb/answer/AnswerService.java
+++ src/main/java/com/sbs/sbb/answer/AnswerService.java
@@ -1,16 +1,37 @@
 package com.sbs.sbb.answer;
 
+import com.sbs.sbb.DataNotException;
 import com.sbs.sbb.question.Question;
 import com.sbs.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AnswerService {
     private final AnswerRepository answerRepository;
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
+    }
 
     public Answer create(Question q, String content, SiteUser author) {
         Answer answer = new Answer();
@@ -22,4 +43,5 @@ public class AnswerService {
 
         return answer;
     }
+
 }
